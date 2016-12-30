62fc0ce4cc80792809370dc18d42094bc8f28e29
Red_Aces <redaces@gmx.de>
2016-12-30 20:41:14 +0100

    add prestige upgrade to ui

--- TrimpsUI.user.js
+++ TrimpsUI.user.js
@@ -10,31 +10,52 @@
 
 window.RedAcesUI = window.RedAcesUI || {};
 
+/** Upgrade efficiency */
+
+window.RedAcesUI.attackAfterPrestige = function(base, prestige) {
+    if (prestige <= 0) {
+        // prestige == 0 => no prestige (so Prestige I)
+        return base;
+    }
+    // prestige == 2 => prestiged 2 times (so Prestige III)
+    return base * 11.51 * Math.pow(9.59, prestige - 1);
+};
+
+window.RedAcesUI.healthAfterPrestige = function(base, prestige) {
+    if (prestige <= 0) {
+        // prestige == 0 => no prestige (so Prestige I)
+        return base;
+    }
+    // prestige == 2 => prestiged 2 times (so Prestige III)
+    return base * 13.61 * Math.pow(11.42, prestige - 1);
+};
+
+/** Equipment efficiency */
+
 window.RedAcesUI.displayEquipEfficiency = function () {
     var costMult = Math.pow(1 - window.game.portal.Artisanistry.modifier, window.game.portal.Artisanistry.level),
         items    = {"Health": [], "Attack": []},
-        itemName;
+        itemName,
+        stat;
 
+    // fill the array with the equipment information
     for (itemName in window.game.equipment) {
         if (!window.game.equipment.hasOwnProperty(itemName)) {
             continue;
         }
         var data = window.game.equipment[itemName],
-            value,
-            stat;
+            gain;
 
-        if (data.locked == 1) {
+        if ((data.locked == 1) || data.hasOwnProperty('blockCalculated')) {
             continue;
         }
 
         if (data.hasOwnProperty('attackCalculated')) {
-            value = data.attackCalculated;
-            stat  = 'Attack';
-        } else if (data.hasOwnProperty('blockCalculated')) {
-            continue;
+            gain = data.attackCalculated;
+            stat = 'Attack';
         } else if (data.hasOwnProperty('healthCalculated')) {
-            value = data.healthCalculated;
-            stat  = 'Health';
+            gain = data.healthCalculated;
+            stat = 'Health';
         } else {
             continue;
         }
@@ -45,16 +66,46 @@ window.RedAcesUI.displayEquipEfficiency = function () {
 
         var resource     = Object.keys(data.cost)[0],
             cost         = data.cost[resource][0] * Math.pow(data.cost[resource][1], data.level) * costMult,
-            costPerValue = cost / value;
+            costPerValue = cost / gain;
 
-        if ((resource != 'metal') || (value == 0)) {
+        if ((resource != 'metal') || (gain == 0)) {
             continue;
         }
 
         items[stat].push({"item": itemName, "costPerValue": costPerValue});
     }
 
-    for (var stat in items) {
+    for (var upgradeName in window.game.upgrades) {
+        if (!window.game.upgrades.hasOwnProperty(upgradeName)) {
+            continue;
+        }
+
+        var upgradeData = window.game.upgrades[upgradeName];
+        if ((upgradeData.locked == 1)
+            || !upgradeData.hasOwnProperty('prestiges')
+            || !window.game.equipment.hasOwnProperty(upgradeData.prestiges)
+            || !upgradeData.hasOwnProperty('cost')
+            || !upgradeData.cost.hasOwnProperty('resources')
+            || !upgradeData.cost.resources.hasOwnProperty('metal')
+        ) {
+            continue;
+        }
+        var equipData = window.game.equipment[upgradeData.prestiges],
+            gain;
+
+        if (equipData.hasOwnProperty('attack')) {
+            stat = 'Attack';
+            gain = window.RedAcesUI.attackAfterPrestige(equipData.attack, upgradeData.done + 1);
+        } else if (equipData.hasOwnProperty('health')) {
+            stat = 'Health';
+            gain = window.RedAcesUI.healthAfterPrestige(equipData.health, upgradeData.done + 1);
+        }
+
+        items[stat].push({"item": upgradeName, "costPerValue": upgradeData.cost.resources.metal * costMult / gain});
+    }
+
+    // sort and display
+    for (stat in items) {
         if (!items.hasOwnProperty(stat)) {
             continue;
         }
